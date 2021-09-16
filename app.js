e6015182f7d5dcdbeb29000d2f27d5d3ecf3b002
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-09-16 13:44:29 +0300

    cleanup

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
 
-const articles = require('./routes/articlesRoute.js');
-const users = require('./routes/usersRoute.js');
 const config = require('./config.js');
 
-const MONGODB_URI = config.mongodburi || 'mongodb://localhost/basic-mern-app';
+const MONGODB_URI = config.mongodburi || 'mongodb://localhost/acphrm';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/basic-mern-app', {
+mongoose.connect('mongodb://localhost:27017/acphrm', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
@@ -24,29 +21,6 @@ mongoose.connection.on('error', (error) => {
 });
 
 let app = express();
-
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.use((req, res, next) => {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-     if (req.method === 'OPTIONS') {
-         res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
-         return res.status(200).json({});
-     }
-     next();
-});
-
-app.use('/api/articles', articles);
-app.use('/api/users', users);
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// });
 
 app.get('/', (req, res) => {
     var path2 = require('path');
--- models/usersModel.js
+++ models/usersModel.js
@@ -5,25 +5,11 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    username: {
+    ma: {
         type: String,
         unique: true,
         required: true
     },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    emailVerified: {
-        type: Boolean,
-        default: false,
-        required: true
-    }
 });
 
 const User = mongoose.model('User', UserSchema)
