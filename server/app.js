ed0506d39abe04010409b6ac487084a8d49a3f7e
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-09-30 12:26:35 +0300

    auth

---
+++ server/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+// set up server
+
+const app = express();
+const PORT = 10004;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://mern-auth-template-tutorial.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
+// connect to mongoDB
+
+mongoose.connect(
+    "mongodb://localhost:27017/acphrm",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
+
+// set up routes
+
+app.use("/auth", require("./routers/userRouter"));
+
+function getLoggedMA(){
+    let mtu = process.env['USERPROFILE']
+    return mtu.slice(mtu.length-7);
+}
+
+
---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  MA: {
+    type: Number,
+    required: true,
+    unique: true
+},
+FirstName: String,
+LastName: String,
+NickName: String,
+CourseNo: Number,
+BirthDate: Date,
+email: String,
+MainPhone: String,
+EmergencyPhone: String,
+AddressCity: String,
+AddressLine: String,
+Rank: String,
+
+Dereg: String,
+Certifications: [mongoose.Schema.Types.ObjectId],
+Team: String,
+fitnesses: [mongoose.Schema.Types.ObjectId],
+opinions: [mongoose.Schema.Types.ObjectId],
+
+passwordHash: { type: String, required: true },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
