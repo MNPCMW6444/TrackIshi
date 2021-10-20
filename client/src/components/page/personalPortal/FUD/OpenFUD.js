fd3d6510efc8314052b16c87e0652c4d9ebb5165
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-10-20 12:55:30 +0300

    angular

--- client/src/components/page/personalPortal/FUD/OpenFUD.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UpdateFUD from './UpdateFUD';
-
-export default function OpenFUD(props) {
-    return (
-        <UpdateFUD suc={props.setSuccessMessage} />
-    );
-}
--- client/src/util/domain.js
+++
@@ -1,4 +0,0 @@
-export default process.env.NODE_ENV === "development"
-  ? "http://localhost:10004"
-  : process.env.NODE_ENV === "production" &&
-    "XofekX";
