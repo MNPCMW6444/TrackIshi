6df6fcd27ca9af760bed4e9248e46c2bc72ce816
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-02-15 20:46:58 +0200

    audit

--- client/src/util/domain.js
+++ client/src/util/domain.js
@@ -1,4 +1,8 @@
-export default process.env.NODE_ENV === "development"
+/* export default process.env.NODE_ENV === "development"
   ? "http://180.212.224.167:10004"
   : process.env.NODE_ENV === "production" &&
-    "http://180.212.224.167:3000";
+    "http://180.212.224.167:3000"; */ //////// לשביל /////////
+
+export default process.env.NODE_ENV === "development"
+  ? "http://localhost:10004"
+  : process.env.NODE_ENV === "production" && "https://trackishi.herokuapp.com";
