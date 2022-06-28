43729cafb05bcea7d0ca77a1295376a80c7fb7e1
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-06-28 20:11:00 +0300

    ji

--- client/src/util/domain.js
+++ client/src/util/domain.js
@@ -1,8 +1,1 @@
-/* export default process.env.NODE_ENV === "development"
-  ? "http://180.212.224.167:10004"
-  : process.env.NODE_ENV === "production" &&
-    "http://180.212.224.167:3000"; */ //////// לשביל /////////
-
-export default process.env.NODE_ENV === "development"
-  ? "http://localhost:10004"
-  : process.env.NODE_ENV === "production" && "https://trackishi.herokuapp.com";
+export default "http://localhost:10004";
